3fb3fa11e638722a4da289d7378c53eb44b073c8
mark novelero <mark.novelero@gmail.com>
2021-04-05 11:59:14 -0400

    commit tree abc50251c62112a81854160a0b5395b899127dca

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+    if(number <= 400){
+      return 'This one is on me!';
+    }
+    if(number > 2000 && number <= 2500){
+      return 'I will gladly take your thirty bucks.';
+    }
+    if(number > 2500){
+      return 'No can do.';
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(string){
+  if(string === 'NYC'){
+    return 'Ok, sounds good.';
+  } else{
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipDescription){
+  if(tipDescription === 'generous'){
+    return 'Thank you so much.';
+  } else if(tipDescription === 'not as generous'){
+    return 'Thank you.';
+  } else{
+    return 'Bye.';
+  }
 }
